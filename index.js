7967e0b8f5862737d42ec8c786cad658f29c43af
Rishi Roy <72419270+roy-rishi@users.noreply.github.com>
2024-03-19 16:59:08 -0700

    jwt auth without refresh token

--- index.js
+++ index.js
@@ -7,36 +7,6 @@ const jwt = require('njwt')
 const app = express();
 const PORT = 3004;
 
-// auth middleware
-function authentication(req, res, next) {
-    const auth_header = req.headers.authorization;
-    console.log(req.headers);
-
-    if (!auth_header) {
-        let err = new Error("unauthenticated request!");
-        res.setHeader("WWW-Authenticate", "Basic");
-        err.status = 401;
-        return next(err);
-    }
-
-    const auth = new Buffer.from (auth_header.split(" ")[1], "base64").toString().split(":");
-    const user = auth[0];
-    const pass = auth[1];
-
-    if (user == "rishi" && pass == "nom") {
-        // authorization success
-        console.log("authorization succeeded");
-        next();
-    } else {
-        let err = new Error("unauthenticated request!");
-        res.setHeader("WWW-Authenticate", "Basic");
-        err.status = 401;
-        return next(err);
-    }
-}
-app.use(authentication);
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(bodyParser.json());
 
 // open db
@@ -51,9 +21,47 @@ app.use(bodyParser.json());
 // db.close();
 
 // validate authorization request, provide token
-app.get('/auth', (req, res) => {
-    console.log('\nauthorizing client ');
-    res.send({"auth-token": "lskjdf"});
+app.get('/login', (req, res) => {
+    console.log("\n/login");
+
+    // parse authentication header
+    const auth_header = req.headers.authorization;
+    if (!auth_header) {
+        let err = new Error("unauthenticated request!");
+        res.setHeader("WWW-Authenticate", "Basic");
+        err.status = 401;
+        return next(err);
+    }
+    const auth = new Buffer.from (auth_header.split(" ")[1], "base64").toString().split(":");
+    const user = auth[0];
+    const pass = auth[1];
+
+    // validate authentication values
+    if (user == "rishi" && pass == "nom") {
+        // authorization success
+        console.log("authorization successful");
+        const claims = {iss: "sortify1.0", sub:"AzureDiamond"};
+        const token = jwt.create(claims, "secret-phrase");
+        token.setExpiration(new Date().getTime() + (60 * 1000));
+        res.send(token.compact());
+    } else {
+        let err = new Error("unauthenticated request!");
+        res.setHeader("WWW-Authenticate", "Basic");
+        err.status = 401;
+        res.send(err);
+    }
+    // res.send({"auth-token": "lskjdf"});
+});
+
+app.get('/verify/:token', (req, res) => {
+    const {token} = req.params;
+    jwt.verify(token, "secret-phrase", (err, verified_jwt) => {
+        if (err) {
+            res.send(err.message);
+        } else {
+            res.send(verified_jwt);
+        }
+    })
 });
 
 app.listen((PORT), () => {
