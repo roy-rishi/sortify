a06b9265806ca5f3197261d50c8566495d455d90
Rishi Roy <72419270+roy-rishi@users.noreply.github.com>
2024-03-20 18:08:30 -0700

    send email verification code

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
-const jwt = require('njwt')
+const jwt = require('njwt');
+require('dotenv').config();
+var nodemailer = require('nodemailer');
 
 const app = express();
 const PORT = 3004;
 
-app.use(bodyParser.json());
+function createJWT(user, expiration_mins) {
+    // add identifying information
+    const claims = { email: user };
+    const token = jwt.create(claims, process.env.SECRET);
+    token.setExpiration(new Date().getTime() + (expiration_mins * 60 * 1000));
+    return token.compact();
+}
+
 
 // open db
 let db = new sqlite3.Database('./db/main.db');
 
 // start server
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen((PORT), () => {
     console.log(`server is running on port ${PORT}`);
+});
+
+// init email service
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS
+    }
 });
 
 // validate login request, provide jwt token
@@ -29,7 +48,7 @@ app.get('/login', (req, res) => {
         err.status = 401;
         return next(err);
     }
-    const auth = new Buffer.from (auth_header.split(" ")[1], "base64").toString().split(":");
+    const auth = new Buffer.from(auth_header.split(" ")[1], "base64").toString().split(":");
     const user = auth[0];
     const pass = auth[1];
 
@@ -37,10 +56,7 @@ app.get('/login', (req, res) => {
     if (user == "rishi" && pass == "nom") {
         // authorization success
         console.log("authorization successful");
-        const claims = {iss: "sortify1.0", sub:"AzureDiamond"};
-        const token = jwt.create(claims, "secret-phrase");
-        token.setExpiration(new Date().getTime() + (60 * 1000));
-        res.send(token.compact());
+        res.send(createJWT(user, 2));
     } else {
         let err = new Error("failed to authenticate!");
         res.setHeader("WWW-Authenticate", "Basic");
@@ -49,15 +65,36 @@ app.get('/login', (req, res) => {
     }
 });
 
-app.get('/verify', (req, res) => {
-    console.log("/verify");
-    const token = req.headers.authorization.toString().split(" ")[1];
-    console.log(token)
-    jwt.verify(token, "secret-phrase", (err, verified_jwt) => {
-        if (err) {
-            res.send(err.message);
+app.post("/verify-email", (req, res) => {
+    console.log("\n/verify-email");
+
+    const email = req.body.email;
+    // create jwt with their email and 15 minute expiration
+    const access_token = createJWT(email, 15);
+
+    var mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: "Verify Your Email to Use Sortify",
+        html: `<h2>Please enter this access code in the Sortify application</h2><p>For your security, we do not provide a direct link.</p><b>${access_token}</b>\n<p>If you did not request to sign up, please disregard this email.</p>`
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
         } else {
-            res.send("verified!");
+            console.log("sent confirmation email", info.response);
+            res.send("confirmation email sent");
         }
-    })
+    });
+});
+
+app.get('/verify', (req, res) => {
+    console.log("\n/verify");
+
+    const token = req.headers.authorization.toString().split(" ")[1];
+    jwt.verify(token, process.env.SECRET, (err, verified_jwt) => {
+        console.log(err ? err.message : "verified jwt");
+        res.send(err ? err.message : "verified!");
+    });
 });
