2f844e7fa7eb5639c3cea3785812e16b1a7ec64d
Rishi Roy <72419270+roy-rishi@users.noreply.github.com>
2024-03-28 23:27:12 -0700

    ability to add and delete filters

--- client/sortify/lib/sorting_page.dart
+++ client/sortify/lib/sorting_page.dart
@@ -3,6 +3,117 @@ import 'package:provider/provider.dart';
 
 import 'app_state.dart';
 
+class SortParameter extends StatefulWidget {
+  const SortParameter({super.key, required this.id, required this.onDelete});
+
+  final int id;
+  final Function(int) onDelete;
+
+  @override
+  State<SortParameter> createState() => _SortParameterState();
+}
+
+class _SortParameterState extends State<SortParameter> {
+  final TextEditingController nameController = TextEditingController();
+  var typeSelected = "Artist";
+  var actionSelected = "Include";
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                child: Row(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 25),
+                          child: SizedBox(
+                            width: 280,
+                            child: TextField(
+                              controller: nameController,
+                              decoration: InputDecoration(labelText: "Name"),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 30, top: 8, bottom: 8),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Type"),
+                          DropdownButton<String>(
+                            value: typeSelected,
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                typeSelected = newValue!;
+                              });
+                            },
+                            items: <String>["Artist", "Album"]
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Action"),
+                          DropdownButton<String>(
+                            value: actionSelected,
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                actionSelected = newValue!;
+                              });
+                            },
+                            items: <String>["Include", "Exclude"]
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15, right: 15),
+                      child: IconButton(
+                          onPressed: () {
+                            print("yo ${widget.id}");
+                            widget.onDelete(widget.id);
+                          },
+                          icon: Icon(Icons.delete_outlined)),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class SortingPage extends StatefulWidget {
   const SortingPage({super.key});
 
@@ -11,6 +122,24 @@ class SortingPage extends StatefulWidget {
 }
 
 class _SortingPageState extends State<SortingPage> {
+  List<SortParameter> filterWidgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // filterWidgets.add(SortParameter(id: 0, onDelete: _deleteFilterWidget));
+  }
+
+  // prevent duplicate IDs
+  var maxId = 0;
+  // remove filter widget
+  void _deleteFilterWidget(int id) {
+    setState(() {
+    filterWidgets.removeWhere((widget) => widget.id == id);
+  });
+  }
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<AppState>();
@@ -24,7 +153,7 @@ class _SortingPageState extends State<SortingPage> {
       child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(40),
+            padding: const EdgeInsets.only(top: 40, bottom: 60),
             child: Text("Configure Sort", style: titleStyle),
           ),
           Expanded(
@@ -33,6 +162,7 @@ class _SortingPageState extends State<SortingPage> {
                 // LEFT PANEL
                 Flexible(
                   child: ListView(
+                    addAutomaticKeepAlives: true,
                     padding: const EdgeInsets.all(8),
                     children: <Widget>[
                       Center(
@@ -45,10 +175,12 @@ class _SortingPageState extends State<SortingPage> {
                         ),
                       ),
                       Center(
-                        child: Text("Add filters to select the songs for sorting"),
+                        child:
+                            Text("Add filters to select the songs for sorting"),
                       ),
+                      // ADD FILTER BUTTON
                       Padding(
-                        padding: const EdgeInsets.only(top:12, bottom:12),
+                        padding: const EdgeInsets.only(top: 12, bottom: 12),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
@@ -61,12 +193,25 @@ class _SortingPageState extends State<SortingPage> {
                                   color: theme.colorScheme.primaryContainer,
                                   child: InkWell(
                                       splashColor: theme.colorScheme.primary,
-                                      onTap: () {},
+                                      onTap: () {
+                                        // add a new SortParameter widget to list
+                                        setState(() {
+                                          filterWidgets.add(SortParameter(
+                                            key: UniqueKey(),
+                                            id: maxId + 1, // create unused ID
+                                            onDelete: _deleteFilterWidget,
+                                          ));
+                                        });
+                                        maxId++;
+                                        print(maxId);
+                                      },
                                       child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
                                         children: [
                                           Text("+",
-                                              style: theme.textTheme.headlineMedium),
+                                              style: theme
+                                                  .textTheme.headlineMedium),
                                           Text("Add Filter"),
                                         ],
                                       )),
@@ -76,6 +221,7 @@ class _SortingPageState extends State<SortingPage> {
                           ],
                         ),
                       ),
+                      ...filterWidgets,
                     ],
                   ),
                 ),
@@ -87,28 +233,23 @@ class _SortingPageState extends State<SortingPage> {
                       SizedBox(
                         height: 60,
                         child: Card(
-                          clipBehavior: Clip.hardEdge,
-                          color: theme.colorScheme.secondaryContainer,
-                          child: Column(
-                            children: [
-                              Text("The Lovue"),
-                              Text("Lorde")
-                            ],
-                          )
-                        ),
+                            clipBehavior: Clip.hardEdge,
+                            color: theme.colorScheme.secondaryContainer,
+                            child: Column(
+                              children: [Text("The Lovue"), Text("Lorde")],
+                            )),
                       ),
                       SizedBox(
                         height: 60,
                         child: Card(
-                          clipBehavior: Clip.hardEdge,
-                          color: theme.colorScheme.secondaryContainer,
-                          child: Column(
-                            children: [
-                              Text("Bite Me"),
-                              Text("Avril Lavigne")
-                            ],
-                          )
-                        ),
+                            clipBehavior: Clip.hardEdge,
+                            color: theme.colorScheme.secondaryContainer,
+                            child: Column(
+                              children: [
+                                Text("Bite Me"),
+                                Text("Avril Lavigne")
+                              ],
+                            )),
                       ),
                     ],
                   ),
